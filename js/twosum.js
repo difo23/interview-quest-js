1ab46ecac31e00a2f8cdba1591c5b29d1ffc3af2
Lizandro <difozanzan@gmail.com>
2020-02-19 22:36:32 -0400

    TwoSum resuelto

--- js/twosum.js
+++ js/twosum.js
@@ -1,17 +1,26 @@
 /**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-	const mins = nums.filter((num) => num <= target);
 
-	return mins.filter((num, i) => {
-		if (mins.includes(target - num)) {
-			return [ i, mins.indexOf(target - num) ];
-		}
-	});
+ 
+var twoSum = function(nums, target) {
+	const mins = nums.filter((num) => nums.includes(target - num));
+	const one = mins[0];
+	const two = mins.find((min) => min + one == target);
+	return [ nums.indexOf(one), nums.indexOf(two) ];
 };
 
-console.log(twoSum([ 2, 7, 11, 15 ], 9));
-// resp. [ 2, 7 ]
+console.log(twoSum([ 2, 7, 7, 11, 7 ], 9));
+// resp. [ 0, 1 ]
---
+++ py/twosum.py
@@ -0,0 +1,2 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
