46bf0d677bff8118bdbc59d7381ba30a3919cc6e
difo23 <diffozanzan@gmail.com>
2020-04-11 23:15:44 -0400

    LinkList Review terminado

--- js/estructurasDatos/LinkeList/LinkList_Review.js
+++ js/estructurasDatos/LinkeList/LinkList_Review.js
@@ -27,6 +27,33 @@ class LinkList {
 		}
 	}
 
+	_decrementPosFrom(pos) {
+		let node = this.head.next;
+		if (!node) return;
+
+		while (pos <= this._length) {
+			if (pos === node.position) {
+				node.position--;
+				pos++;
+			}
+			if (!node.next) return;
+			node = node.next;
+		}
+	}
+
+	_search(index) {
+		if (index > this._length) return false;
+		if (!this.head.next) return false;
+
+		let node = this.head.next;
+
+		while (index - 1 != node.position) {
+			node = node.next;
+			if (!node) return false;
+		}
+		return node;
+	}
+
 	append(value) {
 		/**
          * Append new node in the end of list
@@ -42,20 +69,44 @@ class LinkList {
 		let newNode = new LinkNode(value, this.head.next, 1);
 		this.head.next = newNode;
 		this._length++;
-    }
-    
-    insert(index, value){
+	}
 
-        if(index > this._length) return false;
-        let node = this.head.next;
-        
-        while( index-1 != node.posistion){
-            node = node.next
-        }
-        this._incrementPosFrom(index);
-        let newNode = new LinkNode(value, node.next, index)
-        node.next = newNode;
-    }
+	insert(index, value) {
+		let node = this._search(index);
+		if (!node) return false;
+
+		this._incrementPosFrom(index);
+		let newNode = new LinkNode(value, node.next, index);
+		node.next = newNode;
+		this._length++;
+		return true;
+	}
+
+	remove(index) {
+		let node = this._search(index);
+		if (!node) return false;
+
+		this._decrementPosFrom(index);
+		let prev = node;
+		let del = node.next;
+		let post = del.next;
+		prev.next = post;
+
+		this._length--;
+		return true;
+	}
+
+	showList() {
+		if (!this.head.next) return false;
+		let node = this.head.next;
+		let listMap = new Map();
+
+		while (node) {
+			listMap.set(node.position, node.value);
+			node = node.next;
+		}
+		return listMap;
+	}
 }
 
 let list = new LinkList();
@@ -64,4 +115,6 @@ list.append(4);
 list.append(5);
 list.prepend(6);
 list.prepend(8);
-console.log(list);
+console.log(list.insert(2, 100));
+//list.remove(2);
+console.log(list.showList());
