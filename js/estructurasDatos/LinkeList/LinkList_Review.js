023fd2fbe0f6d769b12f7595319d77c4f82ae54c
difo23 <diffozanzan@gmail.com>
2020-04-11 21:37:01 -0400

    Creo metodo insert index y value

--- js/estructurasDatos/LinkeList/LinkList_Review.js
+++ js/estructurasDatos/LinkeList/LinkList_Review.js
@@ -9,44 +9,59 @@ class LinkNode {
 class LinkList {
 	constructor() {
 		this.head = new LinkNode('HEAD', null, 0);
-		this.tail = head;
+		this.tail = this.head;
 		this._length = 0;
-    }
-    
-    _incrementPosFrom(pos){
-        let node = this.head.next;
-        if(!node) return;
+	}
 
-        while (pos <= this._length){
-            if(pos === node.position){
-              node.position++;
-              pos++;    
-            }
-            if(!nod.next) return;
-            node = node.next;
-            
+	_incrementPosFrom(pos) {
+		let node = this.head.next;
+		if (!node) return;
 
-        }
-
-    }
+		while (pos <= this._length) {
+			if (pos === node.position) {
+				node.position++;
+				pos++;
+			}
+			if (!node.next) return;
+			node = node.next;
+		}
+	}
 
 	append(value) {
 		/**
          * Append new node in the end of list
          */
-        this._length++;
-        let newNode =  new LinkNode(value, null, this._length)
-        this.tail.next = newNode;
-        this.tail = newNode;
-        
-    }
-    
-    prepend(value){
-        this._incrementPosFrom(1);
-        let newNode = new LinkNode(value, this.head.next, 1);
-        this.head.next = newNode;
-        this._length++;
-    }
-    
+		this._length++;
+		let newNode = new LinkNode(value, null, this._length);
+		this.tail.next = newNode;
+		this.tail = newNode;
+	}
 
+	prepend(value) {
+		this._incrementPosFrom(1);
+		let newNode = new LinkNode(value, this.head.next, 1);
+		this.head.next = newNode;
+		this._length++;
+    }
+    
+    insert(index, value){
+
+        if(index > this._length) return false;
+        let node = this.head.next;
+        
+        while( index-1 != node.posistion){
+            node = node.next
+        }
+        this._incrementPosFrom(index);
+        let newNode = new LinkNode(value, node.next, index)
+        node.next = newNode;
+    }
 }
+
+let list = new LinkList();
+list.append(3);
+list.append(4);
+list.append(5);
+list.prepend(6);
+list.prepend(8);
+console.log(list);
