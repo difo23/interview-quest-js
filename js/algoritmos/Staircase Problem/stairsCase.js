073dc573f10840bdeff1946ced4ef9f23876bf63
difo23 <diffozanzan@gmail.com>
2020-09-20 00:12:22 -0400

    Stair Case Way

--- js/algoritmos/Staircase Problem/stairsCase.js
+++ js/algoritmos/Staircase Problem/stairsCase.js
@@ -13,21 +13,33 @@ For example, if N is 4, then there are 5 unique ways:
 What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
  */
 
-let X = [ 1, 2 ];
-let n = 4; 
 
+let X = [1, 3, 5];
+let n = 4;
+
+
+// N =      0 1 2 3 4 5  6
+// Ways =   1 1 2 3 5 8 13
 
 let staircase = (n, X) => {
 
-    let setX = new Set(...X)
-    let cache = [...Array(n+1).fill(0)];
+    let setX = new Set(X)
+    let cache = Array(n + 1).fill(0);
     cache[0] = 1;
 
-    for(let i of [...Array(n+1).keys()]){
-        cache[i] += X.map((x)=>{ i-x}).filter(x => x > 0 ).reduce((a,b)=> a+b)
-        cache[i] = 
+    for (let i = 0; i <= n; ++i) {
+
+        let temp = 0;
+        for (let x of X) {
+            if (i - x > 0) {
+                temp += cache[i - x]
+            }
+        }
+        cache[i] += temp;
+        cache[i] += setX.has(i) ? 1 : 0;
     }
 
-
-
+    return cache.pop();
 }
+
+console.log(staircase(n, X))
--- py/algoritmos/stairsCase/stairCase.py
+++ py/algoritmos/stairsCase/stairCase.py
@@ -1,8 +1,30 @@
 
+# def countWays(n, m):
+
+#     temp = 0
+#     res = [1]
+
+#     for i in range(1, n + 1):
+#         s = i - m - 1
+#         e = i - 1
+#         if (s >= 0):
+#             temp -= res[s]
+#         temp += res[e]
+#         res.append(temp)
+
+#     return res[n]
+
+
+# # Driver Code
+# n = 3
+# m = 2
+
+# print('Number of ways =', countWays(n, m))
+
 
 
 def stairCase(n, X):
-    
+
     cache = [0 for _ in range(n + 1)] # Creo arreglo con 0s
     cache[0] = 1 # el primer elemeto del arreglo lo completo con 1
 
@@ -12,4 +34,4 @@ def stairCase(n, X):
     return cache[-1]
 
 
-print(stairCase(1,[0,2]))
+print(stairCase(4,[1,3, 5]))
